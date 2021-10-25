6b6cfc0f896697d979d63556ddd40525cd1675f4
panos <panos.polis@gmail.com>
2021-10-25 16:44:39 +0300

    - show version in footer

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -9,10 +9,8 @@ class Footer extends Component {
         return <footer className="container-fluid mt-5">
             <div className="container">
                 <div className="row mb-3">
-                    <div className="col align-self-center  p-2 font-weight-bold">
-                        {/*<NavLink className={"text-light"} to={'/sage/settings'}>*/}
-                        {/*    {gettext('About/Contact')}*/}
-                        {/*</NavLink>*/}
+                    <div className="col text-center mt-5 p-2 font-weight-bold">
+                        version: {this.context.rootStore.config.version}
                     </div>
                 </div>
             </div>
