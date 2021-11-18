a626fb6ed24b80bcb36bf9012e65d83e858c687a
panos <panos.polis@gmail.com>
2021-11-18 13:45:37 +0200

    - fix button double action

--- src/components/ui/interactButtons.js
+++ src/components/ui/interactButtons.js
@@ -9,23 +9,24 @@ export default class InteractButton extends Component {
         this.action = this.action.bind(this);
     }
 
-    action(){
-        if(this.props.id){
+    action() {
+        if (this.props.id) {
             this.props.fn(this.props.id);
+        } else {
+            this.props.fn();
         }
-        this.props.fn();
     }
 
     render() {
         return <button className={this.props.css} onClick={() => this.action()}>
-            <FontAwesomeIcon icon={this.props.icon}></FontAwesomeIcon>  {this.props.label}</button>
+            <FontAwesomeIcon icon={this.props.icon}></FontAwesomeIcon> {this.props.label}</button>
     }
 }
 
 InteractButton.propTypes = {
     icon: PropTypes.object,
     css: PropTypes.string,
-    fn:PropTypes.func,
-    label:PropTypes.string,
-    id:PropTypes.number
+    fn: PropTypes.func,
+    label: PropTypes.string,
+    id: PropTypes.number
 }
